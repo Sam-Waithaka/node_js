a17f045f63eeaad1899f90c06cafc27e67c5fac1
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-07 12:23:36 +0300

    Enhance logging in handler to include detailed request information; parse URL components for better diagnostics

--- concurrency/dist/handler.js
+++ concurrency/dist/handler.js
@@ -4,8 +4,16 @@ exports.handler = void 0;
 const console_1 = require("console");
 const handler = async (req, res) => {
     (0, console_1.log)(`HTTP method ${req.method}, URL: ${req.url}`);
-    (0, console_1.log)(`host ${JSON.stringify(req.headers)}`);
+    // log(`host ${JSON.stringify(req.headers)}`)
     (0, console_1.log)(`user-agent: ${req.headers["user-agent"]}`);
+    const parsedURL = new URL(req.url ?? '', `http://${req.headers.host}`);
+    (0, console_1.log)(`protocal: ${parsedURL.protocol}`);
+    (0, console_1.log)(`hostname: ${parsedURL.hostname}`);
+    (0, console_1.log)(`port: ${parsedURL.port}`);
+    (0, console_1.log)(`Pathname: ${parsedURL.pathname}`);
+    parsedURL.searchParams.forEach((val, key) => {
+        (0, console_1.log)(`key ${key} val ${val}`);
+    });
     res.end("Hello, World");
 };
 exports.handler = handler;
--- concurrency/src/handler.ts
+++ concurrency/src/handler.ts
@@ -1,9 +1,20 @@
 import { log } from "console";
 import { IncomingMessage, ServerResponse } from "http";
+import { parse } from "path";
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
     log(`HTTP method ${req.method}, URL: ${req.url}`)
-    log(`host ${JSON.stringify(req.headers)}`)
+    // log(`host ${JSON.stringify(req.headers)}`)
     log(`user-agent: ${req.headers["user-agent"]}`)
+
+    const parsedURL = new URL(req.url ?? '', `http://${req.headers.host}`)
+    log(`protocal: ${parsedURL.protocol}`)
+    log(`hostname: ${parsedURL.hostname}`)
+    log(`port: ${parsedURL.port}`)
+    log(`Pathname: ${parsedURL.pathname}`)
+
+    parsedURL.searchParams.forEach((val, key)=>{
+        log(`key ${key} val ${val}`)
+    })
     
     res.end("Hello, World");
 };
