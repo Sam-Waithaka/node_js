c5fb3aecfbc2e5420dded9c30259f7fc630420ff
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-08 19:58:33 +0300

    feat: enhance readHandler to process JSON payloads and respond based on content type

--- concurrency/dist/readHandler.js
+++ concurrency/dist/readHandler.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
 const stream_1 = require("stream");
 const readHandler = async (req, resp) => {
-    req.pipe(createLowerTransform()).pipe(resp);
+    // req.pipe(createLowerTransform()).pipe(resp)
     // TODO - read request body
     // req.setEncoding('utf-8')
     // req.on('data', (data: string)=>{
@@ -18,10 +18,31 @@ const readHandler = async (req, resp) => {
     // }
     // console.log('End: All data read');
     // resp.end();
+    // JSON transfrom using readableObjectMode
+    if (req.headers['content-type'] == 'application/json') {
+        req.pipe(createFromJsonTransform()).on('data', (payload) => {
+            if (payload instanceof Array) {
+                resp.write(`Reieved an Array with ${payload.length} items`);
+            }
+            else {
+                resp.write('Did not recieve an Array');
+            }
+            resp.end();
+        });
+    }
+    else {
+        req.pipe(resp);
+    }
 };
 exports.readHandler = readHandler;
-const createLowerTransform = () => new stream_1.Transform({
+const createFromJsonTransform = () => new stream_1.Transform({
+    readableObjectMode: true,
     transform(data, encoding, callback) {
-        callback(null, data.toString().toLowerCase());
+        callback(null, JSON.parse(data));
     }
 });
+// const createLowerTransform = ()=> new Transform ({
+//     transform(data, encoding, callback){
+//         callback(null, data.toString().toLowerCase())
+//     }
+// })
--- concurrency/src/readHandler.ts
+++ concurrency/src/readHandler.ts
@@ -2,7 +2,7 @@ import { IncomingMessage, ServerResponse } from "http";
 import { Transform } from "stream";
 export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
 
-    req.pipe(createLowerTransform()).pipe(resp)
+    // req.pipe(createLowerTransform()).pipe(resp)
     // TODO - read request body
     // req.setEncoding('utf-8')
     // req.on('data', (data: string)=>{
@@ -22,10 +22,32 @@ export const readHandler = async (req: IncomingMessage, resp: ServerResponse) =>
     // console.log('End: All data read');
 
     // resp.end();
+
+
+    // JSON transfrom using readableObjectMode
+    if (req.headers['content-type'] == 'application/json'){
+        req.pipe(createFromJsonTransform()).on('data', (payload)=>{
+            if (payload instanceof Array){
+                resp.write(`Reieved an Array with ${payload.length} items`)
+            } else {
+                resp.write('Did not recieve an Array')
+            }
+
+            resp.end()
+        })
+    } else {
+        req.pipe(resp)
+    }
 }
 
-const createLowerTransform = ()=> new Transform ({
+const createFromJsonTransform = ()=> new Transform ({
+    readableObjectMode: true,
     transform(data, encoding, callback){
-        callback(null, data.toString().toLowerCase())
+        callback(null, JSON.parse(data))
     }
 })
+// const createLowerTransform = ()=> new Transform ({
+//     transform(data, encoding, callback){
+//         callback(null, data.toString().toLowerCase())
+//     }
+// })
