92bcbdee3e84027aaf5c5f062de12718521cc01d
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-08 18:32:15 +0300

    feat: implement data reading in readHandler for processing request body

--- concurrency/dist/readHandler.js
+++ concurrency/dist/readHandler.js
@@ -3,6 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.readHandler = void 0;
 const readHandler = (req, resp) => {
     // TODO - read request body
-    resp.end();
+    req.setEncoding('utf-8');
+    req.on('data', (data) => {
+        console.log(data);
+    });
+    req.on('end', () => {
+        console.log('End: All data read');
+        resp.end();
+    });
+    // resp.end();
 };
 exports.readHandler = readHandler;
--- concurrency/src/readHandler.ts
+++ concurrency/src/readHandler.ts
@@ -1,5 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
 export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
     // TODO - read request body
-    resp.end();
+    req.setEncoding('utf-8')
+    req.on('data', (data: string)=>{
+        console.log(data)
+    })
+
+    req.on('end', ()=>{
+        console.log('End: All data read');
+        resp.end()
+        
+    })
+    // resp.end();
 }
