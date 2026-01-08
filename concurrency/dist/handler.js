3cfd8c3a2c892cd30c1998b069f1ad3443e6664f
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-08 16:33:08 +0300

    feat: implement readHandler for processing POST requests and serve index.html

--- concurrency/dist/handler.js
+++ concurrency/dist/handler.js
@@ -1,26 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.basicHandler = void 0;
+const fs_1 = require("fs");
 const basicHandler = (req, resp) => {
-    resp.setHeader('Content-Type', 'text/plain');
-    let i = 0;
-    let canWrite = true;
-    const writeData = () => {
-        console.log("Started writing data");
-        do {
-            canWrite = resp.write(`Message: ${i++}\n`);
-        } while (i < 10_000 && canWrite);
-        console.log("Buffer is at capacity");
-        if (i < 10_000) {
-            resp.once("drain", () => {
-                console.log("Buffer has been drained at: ", i);
-                writeData();
-            });
-        }
-        else {
-            resp.end("End");
-        }
-    };
-    writeData();
+    // resp.setHeader('Content-Type', 'text/plain')
+    // let i = 0;
+    // let canWrite = true;
+    // const writeData = () => {
+    //     console.log("Started writing data"); 
+    //     do {
+    //         canWrite = resp.write(`Message: ${i++}\n`);
+    //     } while (i < 10_000 && canWrite);
+    //     console.log("Buffer is at capacity");
+    //     if (i < 10_000) {
+    //         resp.once("drain", () => {
+    //             console.log("Buffer has been drained at: ", i);
+    //             writeData();
+    //         });
+    //     } else {
+    //         resp.end("End");
+    //     }
+    // }
+    // writeData();
+    resp.write((0, fs_1.readFileSync)("static/index.html"));
+    resp.end();
 };
 exports.basicHandler = basicHandler;
--- concurrency/src/handler.ts
+++ concurrency/src/handler.ts
@@ -2,23 +2,26 @@ import { IncomingMessage, ServerResponse } from "http";
 import { readFileSync } from "fs";
 export const basicHandler = (req: IncomingMessage, resp: ServerResponse) => {
     
-    resp.setHeader('Content-Type', 'text/plain')
-    let i = 0;
-    let canWrite = true;
-    const writeData = () => {
-        console.log("Started writing data"); 
-        do {
-            canWrite = resp.write(`Message: ${i++}\n`);
-        } while (i < 10_000 && canWrite);
-        console.log("Buffer is at capacity");
-        if (i < 10_000) {
-            resp.once("drain", () => {
-                console.log("Buffer has been drained at: ", i);
-                writeData();
-            });
-        } else {
-            resp.end("End");
-        }
-    }
-    writeData();
+    // resp.setHeader('Content-Type', 'text/plain')
+    // let i = 0;
+    // let canWrite = true;
+    // const writeData = () => {
+    //     console.log("Started writing data"); 
+    //     do {
+    //         canWrite = resp.write(`Message: ${i++}\n`);
+    //     } while (i < 10_000 && canWrite);
+    //     console.log("Buffer is at capacity");
+    //     if (i < 10_000) {
+    //         resp.once("drain", () => {
+    //             console.log("Buffer has been drained at: ", i);
+    //             writeData();
+    //         });
+    //     } else {
+    //         resp.end("End");
+    //     }
+    // }
+    // writeData();
+
+    resp.write(readFileSync("static/index.html"));
+    resp.end();
 };
