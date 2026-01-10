8f1c9af6efccb3496e8b55cfe34a74fbe2371f72
Sam-Thakas <sw.waithakasamuel@gmail.com>
2026-01-10 11:24:13 +0300

    feat: implement HTTP proxying for enhanced request handling

--- 07 bundling/dist/server.js
+++ 07 bundling/dist/server.js
@@ -7,8 +7,12 @@ const http_1 = require("http");
 const express_1 = __importDefault(require("express"));
 const readHandler_1 = require("./readHandler");
 const cors_1 = __importDefault(require("cors"));
+const http_proxy_1 = __importDefault(require("http-proxy"));
 const port = 5000;
 const expressApp = (0, express_1.default)();
+const proxy = http_proxy_1.default.createProxyServer({
+    target: 'http://localhost:5100', ws: true
+});
 // expressApp.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -22,6 +26,8 @@ expressApp.use(express_1.default.json());
 expressApp.post("/read", readHandler_1.readHandler);
 expressApp.use(express_1.default.static("static"));
 expressApp.use(express_1.default.static("node_modules/bootstrap/dist"));
-expressApp.use(express_1.default.static("dist/client"));
+// expressApp.use(express.static("dist/client"))
+expressApp.use((req, resp) => proxy.web(req, resp));
 const server = (0, http_1.createServer)(expressApp);
+server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
 server.listen(port, () => console.log(`HTTP Server listening on port ${port}`));
